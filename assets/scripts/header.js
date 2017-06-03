a1c06181653d7816bdbc9bc3edba9a658cc821cd
Tommy Svensson <tommykajsvensson@gmail.com>
2017-06-03 14:17:40 +0200

    mainly animations

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,31 +1,36 @@
 const header = document.querySelector('.header')
 const trigram = header.querySelector('.header__trigram')
+const bannerLogo = header.querySelector('.header__logo-link')
 const navbar = header.querySelector('.navbar')
-const navbarItems = navbar.querySelectorAll('.navbar__item')
-const navbarLinks = navbar.querySelectorAll('.navbar__link')
-// const searchbar = document.querySelectorAll('.searchbar')
+const links = navbar.querySelectorAll('.navbar__link')
 
-// navbarLinks.forEach(x => x.addEventListener('click', (e) => shrinkNavbar(e)))
+trigram.addEventListener('click', expandHeader)
+bannerLogo.addEventListener('click', hideBannerFromLogo)
+links.forEach(x => x.addEventListener('click', hideBanner))
 
-// function shrinkNavbar(e) {
-// 	navbarItems.forEach(x => x.classList.add('navbar__item--shrink'))
-// 	searchbar.forEach(x => x.classList.add('searchbar--fade-out'))
-// 	setTimeout(() => {header.classList.add('header--banner-hidden')}, 600)
-// }
-
-trigram.addEventListener('click', expandNavbar)
-
-function expandNavbar() {
+function expandHeader() {
 	if (header.classList.contains('header--expand')) {
 		header.classList.remove('header--expand')
 		header.classList.add('header--shrink')
-		navbar.classList.remove('navbar--display-navbar')
+		navbar.classList.remove('navbar--display')
+		navbar.classList.add('navbar--hide')
 	} else if (header.classList.contains('header--shrink')) {
 		header.classList.remove('header--shrink')
 		header.classList.add('header--expand')
-		navbar.classList.add('navbar--display-navbar')
+		navbar.classList.remove('navbar--hide')
+		navbar.classList.add('navbar--display')
 	} else {
 		header.classList.add('header--expand')
-		navbar.classList.add('navbar--display-navbar')
+		navbar.classList.add('navbar--display')
 	}
+}
+
+function hideBanner() {
+	header.classList.add('header--shrink')
+	navbar.classList.add('navbar--hide')
+	setTimeout(() => header.classList.add('header--hide'), 600)
+}
+
+function hideBannerFromLogo() {
+	header.classList.add('header--hide')
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,27 @@
 const header = document.querySelector('.header')
 const trigram = header.querySelector('.header__trigram')
-const navbar = header.querySelector('.navbar')
-const navbarItems = navbar.querySelectorAll('.navbar__item')
-const navbarLinks = navbar.querySelectorAll('.navbar__link')
+const navbar = header.querySelector('.navbar-front')
+const navbarItems = navbar.querySelectorAll('.navbar-front__item')
+const navbarLinks = navbar.querySelectorAll('.navbar-front__link')
 const searchbar = document.querySelectorAll('.searchbar')
 
-navbarLinks.forEach(x => x.addEventListener('click', (e) => shrinkNavbar(e)))
+trigram.addEventListener('click', displayNavbar)
+navbarLinks.forEach(x => x.addEventListener('click', shrinkNavbar))
 
-function shrinkNavbar(e) {
-	navbarItems.forEach(x => x.classList.add('navbar__item--shrink'))
+function shrinkNavbar() {
+	navbarItems.forEach(x => x.classList.add('navbar-front__item--shrink'))
 	searchbar.forEach(x => x.classList.add('searchbar--fade-out'))
 	setTimeout(() => {header.classList.add('header--banner-hidden')}, 600)
 }
 
-console.log(window.location.pathname)
-console.log(window.location.pathname.split('/'))
+function displayNavbar() {
+	if (navbar.classList.contains('navbar--display')) {
+		navbar.classList.remove('navbar--display')
+		navbar.classList.add('navbar--hide')
+	} else if (navbar.classList.contains('navbar--hide')) {
+		navbar.classList.remove('navbar--hide')
+		navbar.classList.add('navbar--display')
+	} else {
+		navbar.classList.add('navbar--display')
+	}
+}
